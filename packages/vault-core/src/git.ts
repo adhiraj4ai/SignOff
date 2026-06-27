31adbad5d66c4b41a711ab83c38f7dc77887c389
Rajan Adhikari <adhiraj.rsh@gmail.com>
2026-06-27 10:42:27 +0545

    feat(vault-core): publish complete public API

--- packages/vault-core/src/git.ts
+++ packages/vault-core/src/git.ts
@@ -1,4 +1,4 @@
-import simpleGit from "simple-git";
+import { simpleGit } from "simple-git";
 
 export async function initVaultRepo(vaultPath: string): Promise<void> {
   const git = simpleGit(vaultPath);
--- packages/vault-core/src/index.ts
+++ packages/vault-core/src/index.ts
@@ -1,1 +1,42 @@
-export const VERSION = "0.1.0";
+// Types
+export type {
+  DocumentType,
+  ApprovalAction,
+  ApprovalStatus,
+  ApprovalHistoryEntry,
+  ApprovalRecord,
+  WorkflowConfig,
+  VaultWorkflows,
+  VaultConfig,
+  VaultInfo,
+  VaultsRegistry,
+  PublishResult,
+  CheckApprovalResult,
+} from "./types.js";
+
+// Feature inference
+export { inferFeatureName } from "./feature.js";
+
+// Workflow
+export { readWorkflows, getWorkflowForType } from "./workflow.js";
+
+// Approval
+export {
+  approvalFilePath,
+  readApproval,
+  writeApproval,
+  appendHistory,
+  getApprovalStatus,
+} from "./approval.js";
+
+// Git
+export {
+  initVaultRepo,
+  stageAndCommit,
+  pullLatest,
+  pushToRemote,
+  getHeadSha,
+} from "./git.js";
+
+// VaultManager
+export { VaultManager } from "./vault.js";
